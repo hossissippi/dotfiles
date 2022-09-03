7bacf07ecf248932bc19fe81cceb974c94b4fc86
hossissippi <hossissippi@gmail.com>
2022-09-04 01:45:40 +0900

    plugins.lua

--- plugins.lua
+++ plugins.lua
@@ -200,5 +200,154 @@ require'packer'.startup(function()
       }
     end
   }
+  use { 'ibhagwan/fzf-lua',
+    -- optional for icon support
+    requires = { 'kyazdani42/nvim-web-devicons' }
+  }
+  use { 'junegunn/fzf', run = './install --bin' }
+  vim.api.nvim_set_keymap('n', '<Space>f', "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<Space>b', "<cmd>lua require('fzf-lua').buffers()<CR>", { noremap = true, silent = true })
 
+  -- lsp
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/vim-vsnip"
+
+  require('mason').setup()
+  require('mason-lspconfig').setup_handlers({ function(server)
+    local opt = {
+      -- -- Function executed when the LSP server startup
+      -- on_attach = function(client, bufnr)
+      --   local opts = { noremap=true, silent=true }
+      --   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+      --   vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
+      -- end,
+      capabilities = require('cmp_nvim_lsp').update_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+      )
+    }
+    require('lspconfig')[server].setup(opt)
+  end })
+
+  -- 2. build-in LSP function
+  -- keyboard shortcut
+  vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+  vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+  vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+  vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+  vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+  vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+  vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+  vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+  -- LSP handlers
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }
+  )
+  -- Reference highlight
+  vim.cmd [[
+  set updatetime=500
+  highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+  highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+  highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+  augroup lsp_document_highlight
+  autocmd!
+  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+  augroup END
+  ]]
+
+  -- 3. completion (hrsh7th/nvim-cmp)
+  local cmp = require("cmp")
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+      end,
+    },
+    sources = {
+      { name = "nvim_lsp" },
+      -- { name = "buffer" },
+      -- { name = "path" },
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<C-p>"] = cmp.mapping.select_prev_item(),
+      ["<C-n>"] = cmp.mapping.select_next_item(),
+      ['<C-l>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ["<CR>"] = cmp.mapping.confirm { select = true },
+    }),
+    experimental = {
+      ghost_text = true,
+    },
+  })
+  -- cmp.setup.cmdline('/', {
+  --   mapping = cmp.mapping.preset.cmdline(),
+  --   sources = {
+  --     { name = 'buffer' }
+  --   }
+  -- })
+  -- cmp.setup.cmdline(":", {
+  --   mapping = cmp.mapping.preset.cmdline(),
+  --   sources = {
+  --     { name = "path" },
+  --     { name = "cmdline" },
+  --   },
+  -- })
+
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+  config = function()
+    require('lualine').setup {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = ''},
+        section_separators = { left = '', right = ''},
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        }
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+      winbar = {},
+      inactive_winbar = {},
+      extensions = {}
+    }
+  end
+  }
 end)
+
+
