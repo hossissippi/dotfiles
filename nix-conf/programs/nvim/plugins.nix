76c671e989b77bb163713db94ec649b802f9446c
hosh <hossissippi@gmail.com>
2026-07-01 18:15:05 +0900

    nvim: noice.nvim を追加

--- nix-conf/programs/nvim/plugins.nix
+++ nix-conf/programs/nvim/plugins.nix
@@ -1,5 +1,3 @@
-{ pkgs, ... }:
-{
   plugins = {
     snacks = {
       enable = true;
@@ -227,6 +225,23 @@
         link_roots = true;
         exit_roots = false;
         enable_autosnippets = true;
+      };
+    };
+    noice = {
+      enable = true;
+      settings = {
+        lsp = {
+          override = {
+            "vim.lsp.util.convert_input_to_markdown_lines" = true;
+            "vim.lsp.util.stylize_markdown" = true;
+          };
+        };
+        presets = {
+          bottom_search = true;
+          command_palette = true;
+          long_message_to_split = true;
+          lsp_doc_border = true;
+        };
       };
     };
   };
