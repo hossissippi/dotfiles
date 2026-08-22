07c79ac0568cf2080e0c4937682f13afc6c0b1c7
hosh <hossissippi@gmail.com>
2026-08-22 22:16:56 +0900

    nvim: ステータスラインから LSP クライアント名の表示を削除

--- nix-conf/programs/nvim/plugins.nix
+++ nix-conf/programs/nvim/plugins.nix
@@ -105,27 +105,6 @@
           ];
           lualine_x = [
             "diagnostics"
-            {
-              __unkeyed-1.__raw = ''
-                function()
-                local msg = ""
-                local buf_ft = vim.bo[0].filetype
-                local clients = vim.lsp.get_clients({ bufnr = 0 })
-                if next(clients) == nil then
-                  return msg
-                    end
-                    for _, client in ipairs(clients) do
-                      local filetypes = client.config.filetypes
-                        if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                          return client.name
-                            end
-                            end
-                            return msg
-                            end
-                            '';
-              icon = "";
-              color.fg = "#ffffff";
-            }
           "encoding"
             "fileformat"
             "filetype"
